36f497a9d77f654f468686a0be6c4bef42d83c70
Humdiess <hmzinchannel@gmail.com>
2024-07-04 13:26:51 +0700

    refactor: In progress - Adjusting base URL handling for cross-environment compatibility

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,13 @@
 <?php 
 
-define('BASEURL', 'http://localhost/alpatech-php-mvc/public');
+$base_url = "http://$_SERVER[HTTP_HOST]";
 
-//db
+if (dirname($_SERVER['SCRIPT_NAME']) != '/') {
+    $base_url .= dirname($_SERVER['SCRIPT_NAME']);
+}
+
+define('BASEURL', $base_url);
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
